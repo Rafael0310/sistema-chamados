111f48514201f0b6e6ac9c8161d0e344fec7e694
Rafael Abreu <rafael.abreu031015@gmail.com>
2023-11-04 11:53:16 -0300

    finish profile + add dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,6 +2,11 @@ import { useContext } from "react"
 import { AuthContext } from "../../contexts/auth"
 
 import Header from "../../components/Header"
+import Title from "../../components/Title"
+
+import { FiPlus, FiMessageSquare } from "react-icons/fi"
+
+import { Link } from "react-router-dom"
 
 export default function Dashboard(){
     const {
@@ -15,8 +20,21 @@ export default function Dashboard(){
     return(
         <div>
             <Header />
-            <h1>Dashboard</h1>
-            <button onClick={handleLogout}>Sair</button>
+            <div className="content">
+                <Title name="Tickets">
+                    <FiMessageSquare size={25} />
+                </Title>
+
+                <>
+                    <Link to='/new'>
+                        <FiPlus size={25}/>
+                        Novo Chamado
+                    </Link>
+                    <div className="container">
+
+                    </div>
+                </>
+            </div>
         </div>
     )
 } 
