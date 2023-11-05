d914992b9ba37e55f110cfd4c1184750ffdc76cb
Rafael Abreu <rafael.abreu031015@gmail.com>
2023-11-05 12:01:05 -0300

    create dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -4,7 +4,9 @@ import { AuthContext } from "../../contexts/auth"
 import Header from "../../components/Header"
 import Title from "../../components/Title"
 
-import { FiPlus, FiMessageSquare } from "react-icons/fi"
+import './dashboard.css'
+
+import { FiPlus, FiMessageSquare, FiSearch, FiEdit } from "react-icons/fi"
 
 import { Link } from "react-router-dom"
 
@@ -26,13 +28,38 @@ export default function Dashboard(){
                 </Title>
 
                 <>
-                    <Link to='/new'>
+                    <Link to='/new' className="new">
                         <FiPlus size={25}/>
                         Novo Chamado
                     </Link>
-                    <div className="container">
-
-                    </div>
+                    
+                    <table>
+                        <thead>
+                            <tr>
+                                <th scope="col">Cliente</th>
+                                <th scope="col">Assunto</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Cadastrado em</th>
+                                <th scope="col">#</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td data-label='Cliente'>Mercado Esquina</td>
+                                <td data-label='Assunto'>Suporte</td>
+                                <td data-label='Status'>Em Aberto</td>
+                                <td data-label='Cadastrado'>12/05/2022</td>
+                                <td data-label='#'>
+                                    <button>
+                                        <FiSearch color="#fff" size={25}/>
+                                    </button>
+                                    <button>
+                                        <FiEdit color="#fff" size={25}/>
+                                    </button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </>
             </div>
         </div>
